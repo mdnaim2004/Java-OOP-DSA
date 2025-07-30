a4e37b86569e523642ded87405c0e2b02e1953be
Md Naim <naim.cse2004@gmail.com>
2025-07-30 23:06:26 +0600

    Added 2D Array folder

--- String/Pallendrom_Char.java
+++ String/Pallendrom_Char.java
@@ -1,5 +1,26 @@
+import java.util.Scanner;
+
 public class Pallendrom_Char {
+
+    public static boolean isPallindrom(String name){
+        int n = name.length();
+        for(int i = 0; i < n / 2; i++){
+            if(name.charAt(i) != name.charAt(n - 1 - i)){
+                return false;
+            }
+        }
+        return true;
+    }
+
     public static void main(String[] args) {
-        St
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a word: ");
+        String name = sc.nextLine();
+
+        if(isPallindrom(name)){
+            System.out.println("Palindrome");
+        } else {
+            System.out.println("Not Palindrome");
+        }
     }
 }
